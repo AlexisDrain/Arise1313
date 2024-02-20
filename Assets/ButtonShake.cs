0f7fb22bddb22cf0d5cfb0b1954a061622ca9a4c
Alexis Drain <alexisdrain97@gmail.com>
2024-02-19 16:26:04 -0800

    ui button shake option

---
+++ Assets/ButtonShake.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonShake : MonoBehaviour
+{
+
+    public Vector2 randomRange = new Vector2(-4f, 4f);
+    public float shakeSpeedDefault = 1f;
+
+    private bool shakeButtonState;
+    private Vector3 originalPos;
+    private float shakeSpeedCurrent = 0f;
+    void Start()
+    {
+        originalPos = transform.position;
+    }
+    public void ShakeButtonState(bool newShakeState) {
+        shakeButtonState = newShakeState;
+    }
+
+    void LateUpdate()
+    {
+        if(shakeButtonState == true) {
+
+            if(shakeSpeedCurrent > 0f) {
+                shakeSpeedCurrent -= Time.deltaTime;
+                return;
+            } else {
+                shakeSpeedCurrent = shakeSpeedDefault;
+                transform.position = new Vector3(originalPos.x + Random.Range(randomRange.x, randomRange.y),
+                                                originalPos.y + Random.Range(randomRange.x, randomRange.y),
+                                                originalPos.z);
+            }
+
+        } else {
+            transform.position = originalPos;
+        }
+    }
+}
--- Assets/Scripts/Novel/ChoiceButtonData.cs
+++ Assets/Scripts/Novel/ChoiceButtonData.cs
@@ -8,7 +8,8 @@ public class ChoiceButtonData : MonoBehaviour
 {
     public StoryType storyType;
     public int storyIndex = 0;
-    public GameObject choiceText;
+    public GameObject choicesText;
+    public TextMeshProUGUI buttonText;
 
     void Start()
     {
@@ -19,22 +20,25 @@ public class ChoiceButtonData : MonoBehaviour
     public void UpdateText(string newChoiceText)
     {
         ShowChoice();
-        transform.Find("Text").GetComponent<TextMeshProUGUI>().text = newChoiceText;
+        buttonText.text = newChoiceText;
     }
     public void ShowChoice() {
         GetComponent<Button>().interactable = false;
         GetComponent<Button>().interactable = true; // hack so that the button looks unhighlighted after clicking
+        choicesText.SetActive(true);
         gameObject.SetActive(true);
-        choiceText.SetActive(true);
+        buttonText.gameObject.SetActive(true);
     }
     public void HideChoice() {
         gameObject.SetActive(false);
-        if(storyIndex == 0) { // if there's no first choice. hide the "Choices" text
-            choiceText.SetActive(false);
+        buttonText.gameObject.SetActive(false);
+        choicesText.SetActive(false);
+        if (storyIndex == 0) { // if there's no first choice. hide the "Choices" text
+            buttonText.gameObject.SetActive(false);
         }
     }
     public void ShakeButton(bool newState) {
-        print("shake choice " + newState);
+        GetComponent<ButtonShake>().ShakeButtonState(newState);
     }
     public void ProgressStory() {
         storyType.ProgressNovel(storyIndex);
